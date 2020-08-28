1304620ab37126a193689ae85550951b7bed7b7b
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-08-28 05:14:39 -0500

    Updates

---
+++ precache-manifest.b7f324ba5451412fa2b3423b652be968.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbe137811ea146113998ee7521a61de",
+    "url": "/react-sororityapp/index.html"
+  },
+  {
+    "revision": "462ce8ecd47101c28d44",
+    "url": "/react-sororityapp/static/css/2.5ad4c650.chunk.css"
+  },
+  {
+    "revision": "22ad30b7bfdadfd6476e",
+    "url": "/react-sororityapp/static/css/main.e32b2ffc.chunk.css"
+  },
+  {
+    "revision": "462ce8ecd47101c28d44",
+    "url": "/react-sororityapp/static/js/2.398edb13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-sororityapp/static/js/2.398edb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ad30b7bfdadfd6476e",
+    "url": "/react-sororityapp/static/js/main.1e128615.chunk.js"
+  },
+  {
+    "revision": "617e097a46755f1ea83d",
+    "url": "/react-sororityapp/static/js/runtime-main.d9425f1a.js"
+  },
+  {
+    "revision": "78cf5f9aeb73e52575c9b7ac8e0888d6",
+    "url": "/react-sororityapp/static/media/Mask-Perfil.78cf5f9a.png"
+  },
+  {
+    "revision": "46b17d06e2f22f47c18cd1b80ff0ff25",
+    "url": "/react-sororityapp/static/media/backgroundProfile.46b17d06.png"
+  }
+]);
--- src/components/ButtonRegister.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/components/ButtonResgister.scss';
-
-const ButtonRegister = ({ namebutton }) => (
-  <button className='ButtonRegister' type='submit'>{namebutton}</button>
-);
-
-export default ButtonRegister;
