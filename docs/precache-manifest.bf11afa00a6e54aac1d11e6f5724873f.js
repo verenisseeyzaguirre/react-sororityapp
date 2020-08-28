d96a1822c800627d58ebed640a2c0052a968e73c
verenisseeyzaguirre <verenisse.eyzaguirre@gmail.com>
2020-08-28 04:44:07 -0500

    nombre a docs

---
+++ docs/precache-manifest.bf11afa00a6e54aac1d11e6f5724873f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd401c4542b9e9a8c8ce3f40447440d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462ce8ecd47101c28d44",
+    "url": "/static/css/2.5ad4c650.chunk.css"
+  },
+  {
+    "revision": "af3396e851e172da0e57",
+    "url": "/static/css/main.e32b2ffc.chunk.css"
+  },
+  {
+    "revision": "462ce8ecd47101c28d44",
+    "url": "/static/js/2.398edb13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.398edb13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3396e851e172da0e57",
+    "url": "/static/js/main.3508a64e.chunk.js"
+  },
+  {
+    "revision": "8f5436ab046ffd19ed95",
+    "url": "/static/js/runtime-main.72b4e0ba.js"
+  },
+  {
+    "revision": "78cf5f9aeb73e52575c9b7ac8e0888d6",
+    "url": "/static/media/Mask-Perfil.78cf5f9a.png"
+  },
+  {
+    "revision": "46b17d06e2f22f47c18cd1b80ff0ff25",
+    "url": "/static/media/backgroundProfile.46b17d06.png"
+  }
+]);
